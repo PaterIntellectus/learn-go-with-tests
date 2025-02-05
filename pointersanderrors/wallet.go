e82e572507269764b28052bb2bd0b801f21b133e
PaterIntellectus <nik0121@mail.ru>
2025-02-05 22:01:03 +0500

    refactor(pointersanderrors): end up with wallet and its tests

--- pointersanderrors/wallet.go
+++ pointersanderrors/wallet.go
@@ -1,16 +1,36 @@
 package pointersanderrors
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-type Wallet struct {
-	balance int
+type Bitcoin int
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficiend funds")
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w Wallet) Deposit(amount int) {
-	fmt.Printf("addres of balance in Deposit is %p \n", &w.balance)
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+
+	return nil
 }
